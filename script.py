7a8b48e01ead9019acf88f15dffd4e835c84bd67
marionmorel <marion.mrl@live.fr>
2026-03-28 17:25:55 +0000

    Tasks 1 - 7

--- script.py
+++ script.py
@@ -1,53 +1,61 @@
 # Task 1
 # Write your get_y() function here
+def get_y(m, b, x):
+    y = m * x + b
+    return y
 
 # Uncomment each print() statement to check your work. Each of the following should print True
-#print(get_y(1, 0, 7) == 7)
-#print(get_y(5, 10, 3) == 25)
-
+print(get_y(1, 0, 7) == 7)
+print(get_y(5, 10, 3) == 25)
 
 # Tasks 2 and 3
 # Write your calculate_error() function here
-
+def calculate_error(m, b, point):
+    x_point = point[0]
+    y_point = point[1]
+    expected_y = get_y(m, b, x_point)
+    distance = expected_y - y_point
+    return abs(distance)
 
 # Task 4
 # Uncomment each print() statement and check the output against the expected result
 
 # this is a line that looks like y = x, so (3, 3) should lie on it. thus, error should be 0:
-#print(calculate_error(1, 0, (3, 3)))
+print(calculate_error(1, 0, (3, 3)))
 
 # the point (3, 4) should be 1 unit away from the line y = x:
-#print(calculate_error(1, 0, (3, 4)))
+print(calculate_error(1, 0, (3, 4)))
 
 # the point (3, 3) should be 1 unit away from the line y = x - 1:
-#print(calculate_error(1, -1, (3, 3)))
+print(calculate_error(1, -1, (3, 3)))
 
 # the point (3, 3) should be 5 units away from the line y = -x + 1:
-#print(calculate_error(-1, 1, (3, 3)))
-
+print(calculate_error(-1, 1, (3, 3)))
 
 # Task 5
 # Write your calculate_all_error() function here
-
-
+def calculate_all_error(m, b, points):
+    total_error = 0
+    for point in points:
+        total_error += calculate_error(m, b, point)
+    return total_error
 
 # Task 6
 # Uncomment each print() statement and check the output against the expected result
 datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
 
 # every point in this dataset lies upon y=x, so the total error should be zero:
-#print(calculate_all_error(1, 0, datapoints))
+print(calculate_all_error(1, 0, datapoints))
 
 # every point in this dataset is 1 unit away from y = x + 1, so the total error should be 4:
-#print(calculate_all_error(1, 1, datapoints))
+print(calculate_all_error(1, 1, datapoints))
 
 # every point in this dataset is 1 unit away from y = x - 1, so the total error should be 4:
-#print(calculate_all_error(1, -1, datapoints))
+print(calculate_all_error(1, -1, datapoints))
 
 # the points in this dataset are 1, 5, 9, and 3 units away from y = -x + 1, respectively, so total error should be
 # 1 + 5 + 9 + 3 = 18
-#print(calculate_all_error(-1, 1, datapoints))
-
+print(calculate_all_error(-1, 1, datapoints))
 
 # Tasks 8 and 9
 possible_ms = ['''your list comprehension here''']
